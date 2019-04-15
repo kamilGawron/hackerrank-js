0aeca0204a9ac9831367f4de15ae9dea8ca70782
Kamil Gawron <kgawron955@gmail.com>
2019-04-15 22:59:26 +0200

    add Grading Students

---
+++ Grading Students.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the gradingStudents function below.
+ */
+function gradingStudents(grades) {
+    let arr = [];
+    grades.forEach(function (elem) {
+        if (elem > 37) {
+            if (elem % 5 == 3 || elem % 5 == 4) {
+                arr.push(elem + 5 - elem % 5);
+            }
+            else {
+                arr.push(elem);
+            }
+        }
+        else {
+            arr.push(elem);
+        }
+    })
+    return arr;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    let grades = [];
+
+    for (let gradesItr = 0; gradesItr < n; gradesItr++) {
+        const gradesItem = parseInt(readLine(), 10);
+        grades.push(gradesItem);
+    }
+
+    let result = gradingStudents(grades);
+
+    ws.write(result.join("\n") + "\n");
+
+    ws.end();
+}
