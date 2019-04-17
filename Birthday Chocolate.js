6fe4896b582b9dd5a0cf55c1cba37c576c29fe14
Kamil Gawron <kgawron955@gmail.com>
2019-04-17 14:09:12 +0200

    add Birthday Chocolate

---
+++ Birthday Chocolate.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the birthday function below.
+function birthday(s, d, m) {
+
+    let i, j, suma = 0,
+        licznik = 0;
+    let len = s.length;
+    for (i = 0; i < len; i++) {
+        suma = s[i];
+        for (j = 1; j < m; j++) {
+            suma += s[i + j];
+        }
+        if (suma % d == 0) {
+            licznik++;
+        }
+
+    }
+    return licznik;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const s = readLine().replace(/\s+$/g, '').split(' ').map(sTemp => parseInt(sTemp, 10));
+
+    const dm = readLine().replace(/\s+$/g, '').split(' ');
+
+    const d = parseInt(dm[0], 10);
+
+    const m = parseInt(dm[1], 10);
+
+    const result = birthday(s, d, m);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
